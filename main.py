2233528212d1279fe3f18c732607b8ca59458fbe
Rosh-Py <jharoshan.k54@gmail.com>
2019-10-08 23:44:54 +0530

    bug fixes

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 import sys
 import os
 from PIL import Image
+
 #Take the input[old,new] dir using command line
 #If new dir is not there, then create
 #loop through each image and convert into png and save it in new folder
 
-
 #checking path exists or not
 def validate_path(path):
+    '''
+    checking path exists or not
+    '''
     return os.path.exists(path)
 
 #creating new directory or directories
 def create_dir(path):
+    '''
+    creating new directory or directories
+    '''
     try:
         os.makedirs(path)
+        return True
     except:
         return False
 
 #Function to convert images into png format
 def convert_to_png(src_dir, tgt_dir):
-    try:
-        if validate_path(src_dir):
-            if not validate_path(tgt_dir):
-                create_dir(tgt_dir)
-                list_of_jpg_images = os.listdir(src_dir)
+    '''
+    Function to convert images into png format
+    '''
+    if validate_path(src_dir):
+        if not validate_path(tgt_dir):
+            if create_dir(tgt_dir):
 
-                for file in list_of_jpg_images:
-                    with Image.open(f'{src_dir}/{file}') as img:
-                        name = file.split('.')[0]
-                        if name[1] in ('jpg','jpeg'):
+                try:
+                    list_dir= os.listdir(src_dir) #listing the directory in a list
+
+                    #Looping through each file and saving it in png format
+                    for file in list_dir:
+                        with Image.open(f'{src_dir}/{file}') as img:
+                            name = file.split('.')[0]
                             img.save(f'{tgt_dir}/{name}.png','png')
+                    ######################################################
+                except:
+                    print('Couldn\'t convert to png')
 
+            else:
+                print('Directory couldn\'t be created')
         else:
-            print('Source directory does not exist')
-    except:
-        print('Some error occured')
+            pass
+
+    else:
+        print('Source directory does not exist')
 
 
 
@@ -42,7 +59,6 @@ def main():
     try:
         source_dir = sys.argv[1]
         target_dir = sys.argv[2]
-        # print(source_dir)
         convert_to_png(source_dir,target_dir)
     except:
         print('Source or target directory is not given. Try again!')
